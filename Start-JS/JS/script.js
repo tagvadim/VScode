4390448c9e68bb7f0710664afd8860c59b22b7f5
tagvadim <vadim.tag@mail.ru>
2021-09-21 23:01:28 +0300

    first commit

---
+++ Start-JS/JS/script.js
@@ -0,0 +1,25 @@
+"use strict"
+
+/*let howManyYears = prompt(`укажи сколько тебе лет!`, 18);
+alert(`Мне уже ${howManyYears} лет`);*/
+
+/*let age = confirm("Тебе точно столько лет?");
+if (age == true) {
+   alert ('Да, конечно');
+}else{
+   alert('Я пошутил');
+}  */
+
+/*let nameJs = prompt ('Какое официальное название JavaScript?');
+if (nameJs == "ECMAScript") {
+   alert('Правильно!');
+}else if (nameJs == "Не знаю") {
+   alert('Иди учи')
+}else{
+   alert ('Пока')
+}  */
+
+/*let nameEmployee = prompt ('Вы кто?', 'Директор или сотрудник');
+let kto = (nameEmployee == 'Директор') ? alert ('Здравстуйте!') : (nameEmployee == 'сотрудник') ? alert ('Хай') : (nameEmployee == "") ? alert ('goodbay') : '';  */
+
+
