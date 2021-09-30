de79d59bf927a25cb422d21e911074a5b8a59c03
tagvadim <vadim.tag@mail.ru>
2021-09-30 22:05:28 +0300

    12

--- Start-JS/JS/script.js
+++ Start-JS/JS/script.js
@@ -57,3 +57,131 @@ if (passport == null) {
    }
 }
 */
+
+/*let t = 0;
+while (t < 20){
+   t++;
+   console.log (t);
+} */
+
+/*for (let t = 0; t <= 10; t++)
+  if (t % 2 == 0) {
+  console.log (t);
+}*/
+
+/*let i = 0;
+while (i<3) {
+   i++;
+   console.log (` номер ${i} !`);
+}*/
+
+/* let num;
+do
+   num = prompt("Введите число больше 100", "");
+   while (num <= 100 && num) {
+   alert("верно");
+} */
+
+/*
+const numb = +prompt("Введите число между 0 и 3", "");
+switch (numb) {
+   case 0:
+      alert("Вы ввели ноль");
+      break;
+   case 1:
+      alert("Вы ввели единицу");
+      break;
+   case 2:
+   case 3:
+      alert(" Вы ввели два или три");
+      break;
+}
+*/
+
+/*   function checkAge (age) {
+   return (age > 18) ? true : confirm ("А родители знают?");
+}  */
+
+/*
+function min (a, b) {
+   return (a > b) ? alert (b) : alert (a)
+}
+min (4, 4);
+*/
+
+/*
+function pow (x, n) {
+   if (x < 1 || n < 1) {
+      return alert ("!!!");
+   }
+   let result = (x ** n);
+    alert (result);
+}
+
+let numX = prompt ('Введите натуральное число');
+let numN = prompt ('Введите степень');
+pow (numX, numN);
+*/
+/*
+let propka = (a, b) => a * b;
+console.log (propka (3, 5))  */
+
+
+/*
+let user = {};
+user.name = "Джон";
+console.log(user.name);
+user.surname = "Смит";
+console.log (user.surname);
+user.name = "Петя";
+console.log (user.name);
+delete user.name;
+console.log (user.name);
+*/
+
+///////////////////////////////////////////////////////////////////////////////////////
+/*
+let salaries = {
+   John: 100,
+   Ann: 160,
+   Pete: 130,
+}
+*/
+//неправильное решение 
+/*let sum = salaries.John + salaries.Ann + salaries.Pete;
+if (sum === NaN) {
+   console.log("Объект пустой")
+} else {
+   console.log(sum)
+}*/
+/*
+// правильное решение
+let sum = 0;
+for (let key in salaries) {
+   sum += salaries[key];
+}
+console.log (sum)
+*/
+///////////////////////////////////////////////////////////////////////////////////////
+/*
+let menu = {
+   width: 200,
+   height: 300,
+   title: "My meny",
+};
+
+function multipluNumeric() {
+   for (let size in menu) {
+      if (typeof (menu[size] == "number")) {
+         menu[size] *= 2
+      }
+   }
+}
+
+multipluNumeric(menu);
+console.log(menu.width);
+multipluNumeric(menu);
+console.log(menu.width);
+multipluNumeric(menu);
+console.log(menu.width);
+*/
